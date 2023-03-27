f56551bb78307afef45c6baf4859a6b63ed46fe9
Maenaaa <m.herbomez@rubika-edu.com>
2023-03-27 19:22:13 +0200

    Fleur

---
+++ The Covenant/Assets/Scripts/FlowerBehavior.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBehavior : MonoBehaviour
+{
+    public AnimationClip flower;
+    public AnimationClip flowerout;
+    public Animation anim;
+    public GameObject fleur;
+    public Transform target;
+
+    private void Awake()
+    {
+        Debug.Log("yes");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.clip = flower;
+        anim.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.clip = flowerout;
+        anim.Play();
+    }
+    
+    
+}
