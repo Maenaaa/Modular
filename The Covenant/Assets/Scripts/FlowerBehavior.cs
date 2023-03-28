ba5863fa3770003b8c366ba004fc213f63e3cc00
Maenaaa <m.herbomez@rubika-edu.com>
2023-03-28 15:24:07 +0200

    Fleur2

--- The Covenant/Assets/Scripts/FlowerBehavior.cs
+++ The Covenant/Assets/Scripts/FlowerBehavior.cs
@@ -19,7 +19,8 @@ public class FlowerBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim.clip = flowerout;
+        anim.Play();
     }
 
     // Update is called once per frame
